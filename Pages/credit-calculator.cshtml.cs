9318280cd4a2a7c79a270040b4e9e9846fd5817e
Neychok <n.kalaidjiev@gmail.com>
2022-07-14 21:26:31 +0300

    first commit

---
+++ Pages/credit-calculator.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Finance_Calculator.Pages
+{
+    public class CreditModel : PageModel
+    {
+        private readonly ILogger<CreditModel> _logger;
+        public string SavedHash { get; set; }
+
+        [BindProperty]
+        public int credit_amount { get; set; }
+
+        public CreditModel(ILogger<CreditModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet(string savedHash)
+        {
+            SavedHash = savedHash;
+            credit_amount = 0;
+        }
+
+        public void OnPost()
+        {
+            SavedHash = credit_amount.ToString();
+        }
+
+    }
+}
